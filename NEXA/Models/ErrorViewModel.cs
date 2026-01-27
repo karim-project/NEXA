984c8c481cf81ef9502ae6e714d21229aebb8e2a
كريم محمد <kareemmohamed3165@gmail.com>
2026-01-27 19:40:24 +0200

    Add project files.

---
+++ NEXA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NEXA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
